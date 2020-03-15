05577618d51b6eff1fe588c771c2541bec5b1e2d
Andrea Díaz <andrea.adg97@gmail.com>
2020-03-14 23:00:29 -0600

    PersonalizarProducto activity on progress

--- MobileApp/CoffeeWare/app/src/main/java/don/coffee/coffeeware/PersonalizarProductoActivity.kt
+++ MobileApp/CoffeeWare/app/src/main/java/don/coffee/coffeeware/PersonalizarProductoActivity.kt
@@ -9,4 +9,5 @@ class PersonalizarProductoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personalizar_producto)
     }
+
 }
--- MobileApp/CoffeeWare/app/src/main/java/don/coffee/coffeeware/Producto.kt
+++ MobileApp/CoffeeWare/app/src/main/java/don/coffee/coffeeware/Producto.kt
@@ -1,4 +1,11 @@
 package don.coffee.coffeeware
 
+import java.io.Serializable
+
 data class Producto(var nombre: String,
-                    var image: Int)
+                    var image: Int,
+                    var descripcion: String,
+                    var categoria: Categoria,
+                    var precioBase: Double,
+                    var ingredientesBase: ArrayList<PorcionIngredienteBase>,
+                    var ingredientesExtra: ArrayList<IngredienteExtra>): Serializable
